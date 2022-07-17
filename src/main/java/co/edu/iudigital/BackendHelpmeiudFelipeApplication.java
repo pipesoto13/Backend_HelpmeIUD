b5fc00ee53cae3e79e284a2372192686968f633d
Felipe Soto Correa <f.soto@globant.com>
2022-07-17 14:36:47 -0500

    :tada: Init new project

---
+++ src/main/java/co/edu/iudigital/BackendHelpmeiudFelipeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.iudigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHelpmeiudFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHelpmeiudFelipeApplication.class, args);
+	}
+
+}

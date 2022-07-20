5e90ba4b00004ed257e1129f000fc5c1a7f51e6f
Felipe Soto Correa <f.soto@globant.com>
2022-07-20 14:17:12 -0500

    :construction: model entities

---
+++ src/main/java/co/edu/iudigital/model/Caso.java
@@ -0,0 +1,37 @@
+package co.edu.iudigital.model;
+
+import java.io.Serializable;
+
+public class Caso implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8169358650902209853L;
+	// id INT NOT NULL AUTO_INCREMENT,
+	// PRIMARY KEY(id)
+
+	// fecha_hora DATETIME NULL DEFAULT now(),
+
+	// latitud FLOAT NULL,
+
+	// longitud FLOAT NULL,
+
+	// altitud FLOAT NULL,
+
+	// visible TINYINT NULL DEFAULT 1,
+
+	// descripcion VARCHAR(200) NULL,
+
+	// url_map TEXT NULL,
+
+	// rmi_url TEXT NULL,
+
+	// usuarios_id INT NOT NULL,
+
+	// delitos_id INT NOT NULL,
+
+	// FOREIGN KEY (usuarios_id) REFERENCES usuarios(id),
+	// FOREIGN KEY (delitos_id) REFERENCES delitos(id)
+
+}

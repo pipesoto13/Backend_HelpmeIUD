2207daeb23eaa3597c6a79a1663804b3e901bfc2
Felipe Soto Correa <f.soto@globant.com>
2022-07-30 14:33:57 -0500

    :construction: swagger, email send and config

--- src/main/java/co/edu/iudigital/util/ConstUtil.java
+++ src/main/java/co/edu/iudigital/util/ConstUtil.java
@@ -1,7 +1,7 @@
 package co.edu.iudigital.util;
 
 public interface ConstUtil {
-	public static final String MESSAGE_GENERAL = "Se presentó un erro al procesar la solicitud, ¡vuelva a intentarlo!";
+	public static final String MESSAGE_GENERAL = "Se presentó un error al procesar la solicitud, ¡vuelva a intentarlo!";
 	public static final String MESSAGE_NOT_FOUND = "No existe prospecto";
 	public static final String MESSAGE_ERROR_DATA = "Información no válida";
 	public static final String MESSAGE_ALREADY = "Ya existe prospecto";
